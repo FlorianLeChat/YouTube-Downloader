9ac18ba574c5808d5f7ae3261b442c5778750180
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-03-14 21:26:46 +0100

    Improved advanced parameter checks/generations

--- config.php
+++ config.php
@@ -10,4 +10,28 @@
 
 	// Delete all files in the output folder before downloading a new video.
 	const DONT_KEEP_FILES = true;
+
+	// Available audio formats.
+	const AVAILABLE_VIDEO_FORMATS = [
+		"best" => "Best (default)",
+		"mp4" => "MP4",
+		"webm" => "WebM",
+		"flv" => "FLV",
+		"3gp" => "3GP",
+		"ogg" => "OGG",
+		"mkv" => "MKV",
+		"avi" => "AVI"
+	];
+
+	// Available video formats.
+	const AVAILABLE_AUDIO_FORMATS = [
+		"best" => "Best (default)",
+		"mp3" => "MP3",
+		"m4a" => "M4A",
+		"wav" => "WAV",
+		"flac" => "FLAC",
+		"aac" => "AAC",
+		"opus" => "Opus",
+		"vorbis" => "Vorbis"
+	];
 ?>
